8170c02db8a74614054ecf4f7fe9f6a6c8f88cae
christian-bromann <mail@christian-bromann.com>
2019-08-16 14:23:32 +0200

    finished unit tests

--- packages/devtools/tests/__mocks__/puppeteer-core.js
+++ packages/devtools/tests/__mocks__/puppeteer-core.js
@@ -1,8 +1,54 @@
-export default {
-    connect: jest.fn().mockReturnValue(Promise.resolve({
-        pages: jest.fn().mockReturnValue(Promise.resolve([
-            { close: jest.fn(), url: jest.fn().mockReturnValue('about:blank') },
-            { close: jest.fn(), url: jest.fn().mockReturnValue('http://json.org') }
+const sendMock = jest.fn()
+const listenerMock = jest.fn()
+
+class CDPSessionMock {
+    constructor () {
+        this.send = sendMock
+        this.on = listenerMock
+    }
+}
+
+class PageMock {
+    constructor () {
+        this.on = jest.fn()
+        this.close = jest.fn()
+        this.url = jest.fn().mockReturnValue('about:blank')
+    }
+}
+
+class PageMock2 {
+    constructor () {
+        this.on = jest.fn()
+        this.close = jest.fn()
+        this.url = jest.fn().mockReturnValue('http://json.org')
+    }
+}
+
+class TargetMock {
+    constructor () {
+        this.page = jest.fn().mockImplementation(() => new PageMock())
+        this.createCDPSession = jest.fn().mockImplementation(() => new CDPSessionMock())
+    }
+}
+
+class PuppeteerMock {
+    constructor () {
+        this.waitForTarget = jest.fn().mockImplementation(() => new TargetMock())
+        this.getActivePage = jest.fn().mockImplementation(() => new PageMock()),
+        this.pages = jest.fn().mockReturnValue(Promise.resolve([
+            new PageMock(),
+            new PageMock2()
         ]))
-    }))
+    }
+}
+
+export default {
+    CDPSessionMock,
+    PageMock,
+    TargetMock,
+    PuppeteerMock,
+    sendMock,
+    listenerMock,
+    connect: jest.fn().mockImplementation(
+        () => Promise.resolve(new PuppeteerMock()))
 }
